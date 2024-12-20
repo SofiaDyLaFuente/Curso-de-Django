42057a355844a10ca3427e34facf541870df4505
Sofia Dy La Fuente Monteiro <150842264+SofiaDyLaFuente@users.noreply.github.com>
2024-12-20 13:23:43 -0300

    Inclusão das Generics Views

--- escola2/cursos/urls.py
+++ escola2/cursos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CursoAPIView, AvaliacaoAPIView
-
+from .views import CursosAPIView, CursoAPIView, AvaliacoesAPIView, AvaliacaoAPIView
 #Rotas locais
 urlpatterns = [
-    path('cursos/', CursoAPIView.as_view(), name = 'cursos'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name = 'avaliacoes')
+    path('cursos/', CursosAPIView.as_view(), name = 'cursos'),
+    path('cursos/<int:pk>', CursoAPIView.as_view(), name = 'curso'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name = 'avaliacoes'),
+    path('avaliacao/<int:pk>', AvaliacaoAPIView.as_view(), name = 'avaliacao')
 ]
