8f49c2fbd7c9f79279270410f0d416421f44f731
Sofia Dy La Fuente Monteiro <150842264+SofiaDyLaFuente@users.noreply.github.com>
2024-12-20 09:18:29 -0300

    correção de erros de admin

--- escola/cursos/admin.py
+++ escola/cursos/admin.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 from .models import Curso, Avaliacao
 
 @admin.register(Curso)
-class CursoAdmin(admin.ModelsAdmin):
-    list_display = {"titulo", "url", "criacao", "atualizacao", "ativo"}
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "url", "criacao", "atualizacao", "ativo")
 
-admin.register(Avaliacao)
+@admin.register(Avaliacao)
 class AvaliacaoAdmin(admin.ModelAdmin):
-    list_display = {"curso", "nome", "email", "avalizao", "criacao", "atualizacao", "ativo"}
+    list_display = ("curso", "nome", "email", "avalizao", "criacao", "atualizacao", "ativo")
