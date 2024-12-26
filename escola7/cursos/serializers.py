8defb88252e215020b1211ccd9460304a9af4b17
Sofia Dy La Fuente Monteiro <150842264+SofiaDyLaFuente@users.noreply.github.com>
2024-12-26 08:09:19 -0300

    cópia do projeto para implementação da aula 09 do curso

---
+++ escola7/cursos/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from .models import Curso, Avaliacao
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        # Apenas usuários autenticados (Questões de proteção e segurança)
+        extra_kwargs = {
+            'email': {'write_only':True}
+        }
+        model = Avaliacao
+        fields = (
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo',
+        )
+
+class CursoSerializer(serializers.ModelSerializer):
+    
+    # 1. Nested Relationship
+    # avaliacoes = AvaliacaoSerializer(many = True, read_only = True)
+    
+
+    #2. HyperLinked Related Fields:
+    #(Campo de relacionamento de hyperlink)
+    # avaliacoes = serializers.HyperlinkedRelatedField(Many = True, read_only = True, view_name = 'avaliacao-detail')
+    
+    
+    # 3. Primary Key Related Field
+    avaliacoes = serializers.PrimaryKeyRelatedField(many = True, read_only = True)
+
+    
+    class Meta:
+        model = Curso
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo',
+            'avaliacoes'
+        )
